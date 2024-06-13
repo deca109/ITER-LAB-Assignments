01fa7584e7095fc2f27379295f7fa044da5c934c
Debopriyo Roy <118815246+deca109@users.noreply.github.com>
2024-06-14 05:09:00 +0530

    Update RobinKarpAlgorithm.java

--- 4. Fourth_Semester/Algorithms-Design-2(AD-2)/Lab Class 03/RobinKarpAlgorithm.java
+++ 4. Fourth_Semester/Algorithms-Design-2(AD-2)/Lab Class 03/RobinKarpAlgorithm.java
@@ -2,48 +2,37 @@
  * Ques 2 : Write a Java Program to implement Robin-Karp Algorithm for pattern searching in a string.
  */
 
-public class RobinKarpAlgorithm {
 
-    public static int robinkarp(String textIn, String patternIn) {
-        char[] text = textIn.toCharArray();
-        char[] pattern = patternIn.toCharArray();
-        int n = text.length;
-        int m = pattern.length;
-        int i, j;
-        int prime = 101;
-        int powm = 1;
-        int TextHash = 0, PatternHash= 0;
-        if (m == 0 || m > n) {
-            return -1;
-        }
-        for(i=0; i < m-1; i++) {
-            powm = (powm << 1) % prime;
-        }
-        for(i = 0; i < m; i++) {
-            PatternHash = ((PatternHash << 1) + pattern[i]) % prime;
-            TextHash = ((TextHash << 1) + text[i]) % prime;
-        }
-        for (i = 0; i <= (n-m); i++) {
-            if (TextHash == PatternHash) {
-                for( j = 0; j < m; j++) {
-                    if (text[i+j] != pattern[j]) {
-                        break;
-                    }
-                }
-                if(j == m) return i;
-            }
-            if(i < n-m)
-                TextHash = (((TextHash - text[i] * powm) << 1)+ text[i+m]) % prime;
-            if(TextHash < 0) {
-                TextHash = (TextHash + prime);
-            }
-        }
-        return -1;
-    }
-
-    public static void main(String[] args) {
-        String st1 = "Hello World !";
-        String st2 = "World";
-        System.out.println("Using Brute Force Search pattern found at index : "+robinkarp(st1, st2));
-    }
+public class RabinKarp {
+	public static void main(String args[]) {
+		String text="abbcde";
+		String pattern="bcd";
+		int pl=pattern.length();
+		int p=13;
+		long tHash=generateHash(text.substring(0,pattern.length()),p);
+		long pHash=generateHash(pattern,p);
+		for(int i=pl-1;i<text.length();i++) {
+			if(pHash==tHash) {
+				if(pattern.equals(text.substring(i-pl+1,i+1))) {
+					System.out.println("Pattern Found At Shift "+(i-pl+1));
+					System.exit(0);
+				}
+			}
+			if(i+1<text.length())
+			tHash=updateHash(tHash,p,pl,text.charAt(i-pl+1),text.charAt(i+1));
+		}
+		System.out.println("Pattern Not Found");
+	}
+	static long generateHash(String s,int p) {
+		long hashCode=0;
+		for(int i=0;i<s.length();i++) {
+			hashCode+=(long)(s.charAt(i)*Math.pow(p, i));
+		}
+		return hashCode;
+	}
+	static long updateHash(long tHash,int p,int pl,char oldChar,char newChar) {
+		long hashCode=(tHash-oldChar)/p;
+		hashCode+=newChar*(long)Math.pow(p, pl-1);
+		return hashCode;
+	}
 }
